4c9213ac82d18ddd3bc8086873bdc3d4e2fb868e
Srijan <69349663+Srijan0519@users.noreply.github.com>
2024-07-09 16:00:00 +0530

    assessment

---
+++ New_Aset/app/key.py
@@ -0,0 +1,15 @@
+GROQ_API_KEY= "Your Key"
+
+system_promt_1 = ''' 
+Here is a text:-
+"In recent years, advancements in artificial intelligence have sparked debates about the ethical implications of AI technologies.
+ While AI promises to revolutionize industries and enhance human capabilities, concerns have arisen regarding its impact on employment, 
+ privacy, and societal biases. Critics argue that unchecked AI development could lead to job displacement, invasive surveillance, 
+ and reinforcement of existing inequalities. Proponents, on the other hand, advocate for responsible AI deployment with robust ethical guidelines 
+ to mitigate risks and ensure equitable outcomes for all stakeholders. As AI continues to evolve, 
+addressing these ethical dilemmas remains crucial for fostering trust and maximizing the societal benefits of this transformative technology."
+Summarise this and extract the key ethical concerns and viewpoints expressed.
+
+The user will ask you questions around this concept. If they deviate cleverly nudge them to this context.
+'''
+
